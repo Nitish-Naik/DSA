79c4348b7fa69625d69cf1206737d9dc4d5181ce
Nitish-Naik <126327334+Nitish-Naik@users.noreply.github.com>
2023-11-17 23:46:33 +0530

    Update KadanesAlgorithm.java

--- ALGORITHMS/KadanesAlgorithm.java
+++ ALGORITHMS/KadanesAlgorithm.java
@@ -3,10 +3,8 @@ public class KadanesAlgorithm
     static int maxSubArraySum(int a[])
     {
         int size = a.length;
-        int max_so_far = Integer.MIN_VALUE, max_ending_here
-                                            = 0;
- 
-        for (int i = 0; i < size; i++) {
+        int max_so_far = Integer.MIN_VALUE, max_ending_here= 0;
+         for (int i = 0; i < size; i++) {
             max_ending_here = max_ending_here + a[i];
             if (max_so_far < max_ending_here)
                 max_so_far = max_ending_here;
